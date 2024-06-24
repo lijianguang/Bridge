a90683915608a307365b0ab6427ef2f714a064c8
UDCN\U100251 <jianguang.li@udtrucks.com>
2024-06-24 09:30:37 +0800

    fix build error

--- UnitTest.Bridge/MQTest.cs
+++ UnitTest.Bridge/MQTest.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.ObjectPool;
 using Apache.NMS.ActiveMQ;
 using System;
 using Apache.NMS;
+using System.Reflection.Emit;
 
 namespace UnitTest.Bridge
 {
@@ -42,6 +43,7 @@ namespace UnitTest.Bridge
         public void Test1()
         {
             var tasks = new List<Task>();
+            var threads = new List<Thread>();
             var timer = new Stopwatch();
             timer.Start();
 
@@ -50,91 +52,54 @@ namespace UnitTest.Bridge
 
             for (int i = 0;i < 100; i++)
             {
-                Thread t = new Thread(() =>
+                threads.Add(new Thread(() =>
                 {
-                    var connection = connectionPool.GetAlive();
-                    connectionPool.ReturnDead(connection);
-                });
-                t.SetApartmentState(ApartmentState.STA);
-                t.Start();
-                t.ThreadState 
-                //tasks.Add(Task.Run(() =>
-                //{
-                //    for(int j = 0; j < 1; j++)
-                //    {
-                //        //IConnectionFactory factory = new ConnectionFactory("failover:(tcp://127.0.0.1:61616)");
-                //        //var connection = (Connection)factory.CreateConnection("admin", "admin");
-                //        //connection.Start();
-                //        ////using (ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge))
-                //        ////{
-                //        ////    IDestination destination = session.GetQueue("queue1");
-                //        ////    using (IMessageProducer producer = session.CreateProducer(destination))
-                //        ////    {
-                //        ////        ITextMessage textMessage = producer.CreateTextMessage("test");
-                //        ////        producer.Send(textMessage);
-                //        ////    }
-                //        ////}
-                //        //connection.Stop();
-                //        //connection.Dispose();
+                    for (int j = 0; j < 10; j++)
+                    {
+                        //var connection = connectionPool.GetAlive();
 
-                //        var connection = connectionPool.GetAlive();
-                //        connectionPool.ReturnDead(connection);
-                //    }
-                //}
-                //));
+                        //connectionPool.ReturnDead(connection);
+
+                        IConnectionFactory factory = new ConnectionFactory("failover:(tcp://127.0.0.1:61616)");
+                        var connection = (Connection)factory.CreateConnection("admin", "admin");
+                        connection.Start();
+                        //using (ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge))
+                        //{
+                        //    IDestination destination = session.GetQueue("queue1");
+                        //    using (IMessageProducer producer = session.CreateProducer(destination))
+                        //    {
+                        //        ITextMessage textMessage = producer.CreateTextMessage("test");
+                        //        producer.Send(textMessage);
+                        //    }
+                        //}
+                        connection.Stop();
+                        connection.Dispose();
+                    }
+                }));
+            }
+
+            threads.ForEach(t =>
+            {
+                t.Start();
+            });
+            checkAlive:
+            var existAlive = false;
+            threads.ForEach(t =>
+            {
+                if(t.ThreadState != System.Threading.ThreadState.Stopped)
+                {
+                    existAlive = true;
+                }
+            });
+            if (existAlive)
+            {
+                goto checkAlive;
             }
 
             Task.WhenAll(tasks).Wait();
             TimeSpan timeTaken = timer.Elapsed;
             string foo = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
             Console.WriteLine(foo);
-            return;
-            var publisher = _serviceProvider.GetRequiredService<IPublisher>();
-            //var tasks = new List<Task>();
-
-            //var result = publisher.PublishAndWaitReplyAsync<MsgTmp, MsgTmp>(MQType.ActiveMQ, MQNames.Queue2, "Test1", new MsgTmp { Name = "A", Age = 1 }).Result;
-
-            //return;
-            for (int i = 0; i < 1; i++)
-            {
-                //tasks.Add(Task.Run(() =>
-                //{
-                //    var result = publisher.PublishAndWaitReplyAsync<MsgTmp, MsgTmp>(MQType.ActiveMQ, MQNames.Queue2, "Test1", new MsgTmp { Name = "A", Age = 1 }).Result;
-
-                //}));
-                tasks.Add(Task.Run(() =>
-                {
-                    publisher.PublishAsync(MQType.ActiveMQ, MQNames.Queue2, "Test2", new MsgTmp { Name = "A", Age = 1 });
-                }));
-                tasks.Add(Task.Run(() =>
-                {
-                    publisher.PublishAsync(MQType.ActiveMQ, MQNames.Queue1, "Test3");
-                }));
-                tasks.Add(Task.Run(() =>
-                {
-                    publisher.PublishAsync(MQType.ActiveMQ, MQNames.Queue1, "Test4");
-                }));
-                tasks.Add(Task.Run(() =>
-                {
-                    publisher.PublishAsync(MQType.ActiveMQ, MQNames.Queue1, "Test5", 5);
-                }));
-                tasks.Add(Task.Run(() =>
-                {
-                    publisher.PublishAsync(MQType.ActiveMQ, MQNames.Queue1, "Test6");
-                }));
-                tasks.Add(Task.Run(() =>
-                {
-                    publisher.PublishAsync(MQType.ActiveMQ, MQNames.Queue1, "Test7", 5);
-                }));
-                tasks.Add(Task.Run(() =>
-                {
-                    publisher.PublishAsync(MQType.ActiveMQ, MQNames.Queue1, "Test8", 9);
-                }));
-            }
-
-            Task.WhenAll(tasks).Wait();
-
-            Thread.Sleep(100000);
         }
     }
 }
