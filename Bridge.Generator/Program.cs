77df9d8d28561c2c3ea1779d79374680ca4a5790
UDCN\U100251 <jianguang.li@udtrucks.com>
2025-02-12 15:26:46 +0800

    remove unnecessary

--- Bridge.Generator/Program.cs
+++ Bridge.Generator/Program.cs
@@ -10,13 +10,11 @@ namespace Bridge.Proxy.Generator
             var generator1 = new ProxyGenerator();
             generator1.SetNamespacePrefix("Sub1")
                 .Generate(typeof(Queue1Handler).Assembly,
-                "C:\\Study\\Bridge\\Bridge.Proxy\\Sub1Proxies",
                 "C:\\Study\\Bridge\\Bridge.Pub\\Sub1Proxies");
 
             var generator2 = new ProxyGenerator();
             generator2.SetNamespacePrefix("Sub2")
                 .Generate(typeof(Bridge.Sub1.Queue3Handler).Assembly,
-                "C:\\Study\\Bridge\\Bridge.Proxy\\Sub2Proxies",
                 "C:\\Study\\Bridge\\Bridge.Pub\\Sub2Proxies");
         }
     }
